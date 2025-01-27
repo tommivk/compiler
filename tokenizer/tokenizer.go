00db1962a5d486b37f1923920611f1122fd1f51b
tommivk <tommikokk@gmail.com>
2025-01-27 17:54:28 +0200

    use struct for tokens and add location

--- tokenizer/tokenizer.go
+++ tokenizer/tokenizer.go
@@ -2,14 +2,33 @@ package tokenizer
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 )
 
-func CheckWhiteSpace(s string) int {
-	re := regexp.MustCompile(`(^\s*)`)
+type Location struct {
+	Line   int
+	Column int
+}
+
+type Token struct {
+	Text string
+	Type string
+	Location
+}
+
+func CountWhitespace(s string) int {
+	re := regexp.MustCompile(`^[^\S\r\n]`)
 	result := re.Find([]byte(s))
 	whitespace := len(string(result))
 	return whitespace
+}
+
+func CountNewlines(s string) int {
+	re := regexp.MustCompile(`^[\r\n]+`)
+	result := re.Find([]byte(s))
+	newLines := len(string(result))
+	return newLines
 }
 
 func ExtractNextToken(s string) string {
@@ -18,43 +37,66 @@ func ExtractNextToken(s string) string {
 	return string(token)
 }
 
-func MatchRegexes(s string) error {
-	// Check integers
+func MatchInteger(s string) bool {
 	re := regexp.MustCompile(`^[0-9]*$`)
 	result := re.Find([]byte(s))
 	integer := string(result)
-	if len(integer) > 0 {
-		return nil
-	}
-
-	// Check variable names and keywords `[^\s]+`
-	re = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z_0-9]*`)
-	result = re.Find([]byte(s))
-	word := string(result)
-	if len(word) > 0 {
-		return nil
-	}
-
-	return errors.New("error while matching regex")
+	return len(integer) > 0
 }
 
-func Tokenize(s string) ([]string, error) {
-	tokens := []string{}
+func MatchIdentifiers(s string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z_][a-zA-Z_0-9]*`)
+	result := re.Find([]byte(s))
+	word := string(result)
+	return len(word) > 0
+}
+
+func MatchRegexes(s string) (Token, error) {
+	if MatchInteger(s) {
+		return Token{Type: "integer", Text: s}, nil
+	}
+
+	if MatchIdentifiers(s) {
+		return Token{Type: "identifier", Text: s}, nil
+	}
+
+	return Token{}, errors.New("error while matching regex")
+}
+
+var line int
+var column int
+
+func Tokenize(s string) ([]Token, error) {
+	tokens := []Token{}
+	line = 0
+	column = 0
 	for i := 0; i < len(s); i++ {
-		whitespace := CheckWhiteSpace(s[i:])
+		newLines := CountNewlines(s[i:])
+		if newLines > 0 {
+			line += newLines
+			column = 0
+			i += newLines - 1
+			continue
+		}
+
+		whitespace := CountWhitespace(s[i:])
 		if whitespace > 0 {
+			column += whitespace
 			i += whitespace - 1
 			continue
 		}
 
-		token := ExtractNextToken(s[i:])
-		err := MatchRegexes(token)
+		input := ExtractNextToken(s[i:])
+		token, err := MatchRegexes(input)
 		if err != nil {
 			return nil, err
 		}
-
+		token.Location = Location{Line: line, Column: column}
 		tokens = append(tokens, token)
-		i += len(token)
+		fmt.Println(token)
+		i += len(token.Text) - 1
+		column += len(token.Text)
 	}
+
 	return tokens, nil
 }
